a742bececfbbd449cdd9acf22e9453283ad74d7f
Justice <justiceodia@cmail.carleton.ca>
2021-02-12 11:56:38 -0500

    Added Tests for Travis CI

---
+++ src/main/java/com/example/demo/TestingWebApplicationTests.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+import org.junit.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class TestingWebApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+}
