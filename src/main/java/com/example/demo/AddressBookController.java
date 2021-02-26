a041bcd4deff24e99ab0bf15ee37051220ec29c7
Justice <justiceodia@cmail.carleton.ca>
2021-02-26 18:30:51 -0500

    Added an SPA

--- src/main/java/com/example/demo/AddressBookController.java
+++ src/main/java/com/example/demo/AddressBookController.java
@@ -4,66 +4,103 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.ArrayList;
 
-@Controller
+@RestController
 public class AddressBookController {
 
     @Autowired
     private AddressBookRepository repository;
 
     @GetMapping("/")
-    public String home() {
-        return "home";
+    public ModelAndView home() {
+        return new ModelAndView("home");
     }
 
     @GetMapping("/index")
-    public String index(Model model) {
+    public ModelAndView index(Model model) {
         Iterable<AddressBook> it = repository.findAll();
         ArrayList<AddressBook> ab = new ArrayList<AddressBook>();
         for(AddressBook e: it){
             ab.add(e);
         }
         model.addAttribute("ab",ab);
-        return "AddressBook/index";
+        return new ModelAndView("AddressBook/index");
     }
 
+    @GetMapping("/json/index")
+    public ArrayList<AddressBook> indexJson() {
+        Iterable<AddressBook> it = repository.findAll();
+        ArrayList<AddressBook> ab = new ArrayList<AddressBook>();
+        for(AddressBook e: it){
+            ab.add(e);
+        }
+        return ab;
+    }
+
+    @GetMapping("/json/create")
+    public AddressBook createJSON() {
+        AddressBook addressBook = new AddressBook();
+        repository.save(addressBook);
+        return addressBook;
+    }
+
+
+
     @GetMapping("/create")
-    public String create(Model model) {
+    public ModelAndView create(Model model) {
         AddressBook addressBook = new AddressBook();
         repository.save(addressBook);
         model.addAttribute(addressBook);
-        return "AddressBook/new";
+        return new ModelAndView("AddressBook/new");
     }
 
     @GetMapping("/{id}")
-    public String showAddressBook(@PathVariable String id, Model model) {
+    public ModelAndView showAddressBook(@PathVariable String id, Model model) {
         AddressBook addressBook = repository.findById(Long.parseLong(id));
         model.addAttribute(addressBook);
-        return "AddressBook/show";
+        return new ModelAndView("AddressBook/show");
     }
 
+    @GetMapping("json/{id}")
+    public AddressBook jsonShowAddressBook(@PathVariable String id) {
+        return repository.findById(Long.parseLong(id));
+    }
+
+    @GetMapping("/spa")
+    public ModelAndView singlePageApplication() {
+        return new ModelAndView("spa");
+    }
 
     @GetMapping("/{id}/new")
-    public String newBuddyForm(@PathVariable String id, Model model) {
+    public ModelAndView newBuddyForm(@PathVariable String id, Model model) {
         model.addAttribute("buddy", new BuddyInfo());
         model.addAttribute("id",id);
-        return "BuddyInfo/new";
+        return new ModelAndView("BuddyInfo/new");
     }
 
     @PostMapping("/new")
-    public String newBuddySubmit(@RequestParam(name = "id") String id, @ModelAttribute BuddyInfo buddy, Model model) {
+    public ModelAndView newBuddySubmit(@RequestParam(name = "id") String id, @ModelAttribute BuddyInfo buddy, Model model) {
         AddressBook addressBook = repository.findById(Long.parseLong(id));
         addressBook.addBuddies(buddy);
         repository.save(addressBook);
         model.addAttribute("buddy", buddy);
         model.addAttribute(addressBook);
-        return "BuddyInfo/show";
+        return new ModelAndView("BuddyInfo/show");
+    }
+
+    @PostMapping("spa/json/new")
+    public BuddyInfo jsonBuddySubmit(@RequestParam(name = "id") String id, @RequestBody BuddyInfo buddy) {
+        AddressBook addressBook = repository.findById(Long.parseLong(id));
+        addressBook.addBuddies(buddy);
+        repository.save(addressBook);
+        return buddy;
     }
 
     @GetMapping("/{id}/{buddyId}")
-    public String showBuddy(@PathVariable String id, @PathVariable String buddyId,  Model model) {
+    public ModelAndView showBuddy(@PathVariable String id, @PathVariable String buddyId,  Model model) {
         AddressBook addressBook = repository.findById(Long.parseLong(id));
         for(BuddyInfo b: addressBook.getBuddies()){
             if(b.getId() == Long.parseLong(buddyId)){
@@ -71,7 +108,7 @@ public class AddressBookController {
             }
         }
         model.addAttribute("addressBook",addressBook);
-        return "BuddyInfo/show";
+        return new ModelAndView("BuddyInfo/show");
     }
 
 
